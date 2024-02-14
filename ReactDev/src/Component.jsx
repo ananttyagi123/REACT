505048768f23464ab962123bc96fd353abed2bc4
Anant Tyagi <ananttyagi359@gmail.com>
2024-02-14 12:35:44 +0530

    updated component

--- ReactDev/src/Component.jsx
+++ ReactDev/src/Component.jsx
@@ -4,13 +4,13 @@ function Component() {
   return (<>
     <div className="flex flex-row" >
       <div className="h-80 w-40 bg-slate-800 border-x-amber-300 border-4 hover:border-rounded-lg m-3" >
-        <h1>Component in React.JS</h1>
+        <h1 className='text-white'>Component in React.JS</h1>
       </div>
       <div className="h-80 w-40 bg-slate-800 border-x-amber-300 border-4  hover:border-rounded-lg m-3" >
-        <h1>Component in React.JS</h1>
+        <h1 className='text-white'>Component in React.JS</h1>
       </div>
       <div className="h-80 w-40 bg-slate-800 border-x-amber-300 border-4  hover:border-rounded-lg m-3" >
-        <h1>Component in React.JS</h1>
+        <h1 className='text-white'>Component in React.JS</h1>
       </div>
     </div>
   </>);
