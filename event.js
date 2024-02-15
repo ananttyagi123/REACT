b6e0b9b03ade5ca5d7ea740e1b105c800d577464
Anant Tyagi <ananttyagi359@gmail.com>
2024-02-15 23:41:29 +0530

    event Listener

---
+++ event.js
@@ -0,0 +1,19 @@
+
+// Get the element with ID 'keyevent1'
+const keyevents = document.getElementById('keyevent');
+;
+// Function to handle keydown events
+
+// Update the inner HTML of 'keyevent1' 
+var color =[]
+keyevents.addEventListener('click', function () {
+  this.style.background = 'yellow';
+
+  console.log("event listener");
+})
+
+// Add event listener for 'keydown' event on 'keyevent1' element
+
+
+
+
